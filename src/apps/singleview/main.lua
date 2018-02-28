e93044e7280dd8aac9e917bdb3628ab7b95eeb35
Charlie Turner <chturne@gmail.com>
2018-02-28 10:52:56 +0000

    Enable MSE by default.
    The GTK minibrowser generates option strings for all the available WK
    web settings, which is convenient and perhaps would be nice for dyz one
    day too. MSE should be defaulted on these days, such discussion has been
    going on upstream, and possibly even merged by now.

--- src/apps/singleview/main.lua
+++ src/apps/singleview/main.lua
@@ -83,6 +83,7 @@ function module.run(args)
                         "allow-file-access-from-file-urls", true,
                         "allow-universal-access-from-file-urls", true,
                         "enable-write-console-messages-to-stdout", true,
+                        "enable-mediasource", true,
                         nil)
     local view = glib.g_object_new(wpe.webkit_web_view_get_type(),
                         "backend", view_backend,
